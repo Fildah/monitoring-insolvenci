5e6ade3a3e8d7d072047f36b27ddfd70c594f00f
Fildah <filip.hans2@gmail.com>
2021-07-28 15:59:59 +0200

    added partner_detail

--- sledovacinsolvenci/static/js/user_insolvencies.js
+++ sledovacinsolvenci/static/js/user_insolvencies.js
@@ -20,8 +20,8 @@ function format(data) {
         '<div class="card-body">' +
         '<h5 class="card-title">Obecné informace</h5>' +
         '<ul class="list-group list-group-flush">' +
-        '<li class="list-group-item">Založeno: ' + data.business_start + '</li>' +
-        '<li class="list-group-item">Ukončeno: ' + data.business_end + '</li>' +
+        '<li class="list-group-item">Datum vzniku: ' + data.business_start + '</li>' +
+        '<li class="list-group-item">Datum zániku: ' + data.business_end + '</li>' +
         '<li class="list-group-item">Stav podnikání: ' + data.state + '</li>' +
         '<li class="list-group-item">Právní forma: ' + data.business_form + '</li>' +
         '</ul>' +
